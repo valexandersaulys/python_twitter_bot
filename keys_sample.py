25ce5a8207b4be920e127b2213d92d2c0a3240e4
Vincent Alexander Saulys <valexandersaulys@gmail.com>
2014-11-17 23:15:38 -0500

    Added a smaple

---
+++ keys_sample.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#keys.py
+#visit http://dev.twitter.com to create an application and get your keys
+keys = dict(
+ CONSUMER_KEY = '', # keep the quotes, replace this with your consumer key
+ CONSUMER_SECRET = '',  # keep the quotes, replace this with your consumer secret key
+ ACCESS_KEY = '',  # keep the quotes, replace this with your access token
+ ACCESS_SECRET = ''  # keep the quotes, replace this with your access token secret
+)
